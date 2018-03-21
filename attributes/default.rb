f6975aa0809150d49ca1c14274b29358ce69665b
Anshul Somani <anshul.somani@soltius.nz>
2018-03-22 12:19:20 +1300

    fixed case statement

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,9 +6,9 @@ default['efs']['retrans'] = 2
 default['efs']['mounts'] = {}
 default['efs']['remove_unspecified_mounts'] = false
 
-default['efs']['nfs-package'] = case node['platform']
-                                when 'ubuntu', 'debian'
-                                  'nfs-common'
-                                when 'redhat', 'amazon', 'oracle', 'suse'
-                                  'nfs-utils'
-                                end
+case node['platform']
+when 'ubuntu', 'debian'
+  default['efs']['nfs-package'] = 'nfs-common'
+when 'redhat', 'amazon', 'oracle', 'suse'
+  default['efs']['nfs-package'] = 'nfs-utils'
+end
